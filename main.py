27352ec2db66c2d37343b1ca9cd1b08b65b4e002
Minh <minh020698@gmail.com>
2022-03-21 08:36:37 +0000

    Add API key

---
+++ main.py
@@ -0,0 +1,7 @@
+from binance.spot import Spot as Client
+
+key='3nM52scGguNWEC2yyImui4CWWohYJT5t6oIyLohd9vSeznRbKflBb378qcpNs4Sx'
+secret='XG944FnF7FmFbwIKgMNQRKLMIuzOezkp7lXbHPJ4HIiDGZvqAwdUphYEgurux1UW'
+
+client = Client(key, secret, base_url='https://testnet.binance.vision')
+print(client.account())
