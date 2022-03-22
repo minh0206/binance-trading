68e7700c5dfea54c50a03ab6dae96af6ac6b7c60
Minh <minh020698@gmail.com>
2022-03-22 09:56:13 +0000

    Add price and balance

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ key='3nM52scGguNWEC2yyImui4CWWohYJT5t6oIyLohd9vSeznRbKflBb378qcpNs4Sx'
 secret='XG944FnF7FmFbwIKgMNQRKLMIuzOezkp7lXbHPJ4HIiDGZvqAwdUphYEgurux1UW'
 
 client = Client(key, secret, base_url='https://testnet.binance.vision')
-print(client.account())
+
+print(*client.ticker_price(), sep='\n')
+print()
+print(*client.account()['balances'], sep='\n')
